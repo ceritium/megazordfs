f366b4483e4179d06c7a1ddaa023d59d5ef6e75c
Jose Galisteo <ceritium@gmail.com>
2020-09-25 18:51:09 +0200

    new blocks strategy

--- test/volumeDevice.js
+++ test/volumeDevice.js
@@ -1,34 +1,76 @@
 const assert = require('assert')
-const { describe, it, before, after } = require('mocha')
+const { it } = require('mocha')
 
-const { stats, virtualDeviceSize } = require('../lib/volumeDevice')
+const { stats, virtualDeviceSize, smallerStripe, buildPool } = require('../lib/volumeDevice')
 
 it('stats', () => {
-  console.log(stats(['.']))
+  stats(['.'])
+})
+
+it('buildPool', () => {
+  const blockA = { total: 100 }
+  const blockB = { total: 200 }
+  const blockC = { total: 300 }
+  const blocks = [blockA, blockB, blockC]
+
+  const poolA = buildPool(blocks, 1)
+  assert.deepStrictEqual(poolA, [[blockC, blockB, blockA]])
+
+  const poolB = buildPool(blocks, 2)
+  assert.deepStrictEqual(poolB, [[blockC], [blockB, blockA]])
+
+  const poolC = buildPool(blocks, 3)
+  assert.deepStrictEqual(poolC, [[blockC], [blockB], [blockA]])
+
+  const poolD = buildPool(blocks, 4)
+  assert.deepStrictEqual(poolD, [[blockC], [blockB], [blockA], []])
+})
+
+it('smallerStripe', () => {
+  const stripeA = [
+    { total: 100 },
+    { total: 220 },
+    { total: 220 }
+  ]
+
+  const stripeB = [
+    { total: 2 },
+    { total: 10 }
+  ]
+
+  const stripeC = [
+    { total: 200 }
+  ]
+
+  const pool = [stripeA, stripeB, stripeC]
+  const res = smallerStripe(pool)
+
+  assert.strictEqual(res, stripeB)
 })
 
 it('virtualDeviceSize', () => {
-  const blockStats = {
-    '/tmp/BLOCK1': {
+  const blockStats = [
+    {
       block: '/tmp/BLOCK1',
       available: 100,
       free: 200,
       total: 300
     },
-    '/tmp/BLOCK2': {
+    {
       block: '/tmp/BLOCK2',
       available: 100,
       free: 500,
       total: 600
     },
-    '/tmp/BLOCK3': {
+    {
       block: '/tmp/BLOCK3',
       available: 100,
       free: 100,
       total: 200
     }
-  }
-  const virtualDevice = virtualDeviceSize(blockStats)
+  ]
+
+  const virtualDevice = virtualDeviceSize(blockStats, 2)
   assert.strictEqual(virtualDevice.totalSize(), 500)
   assert.strictEqual(virtualDevice.freeSpace(), 300)
 })
